011a093b4e3fd9e95f07d9db91620dd3c0c1eda7
Aravind Vemula <aravind@kiprosh.com>
2019-08-29 13:25:05 +0530

    update: winning score ➤ 💯

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
         document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
         // 3. check if player won the game
-        if(scores[activePlayer] >= 10) {
+        if(scores[activePlayer] >= 100) {
             document.querySelector('#name-' + activePlayer).textContent = 'WINNER!';
             document.querySelector('.dice').style.display = 'none';
             document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
